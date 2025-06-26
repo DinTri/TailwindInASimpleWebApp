3e2562a8c6b7b851d3b35ecfea060f09485885b7
Trifon Dinev <tpd_bg@yahoo.com>
2025-06-27 01:28:50 +0300

    Stop tracking history

--- .history/src/js/flip_20250623000339.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const flipCard = document.getElementById('flipCard')
-  document.querySelectorAll('[data-toggle-flip]').forEach((el) => {
-    el.addEventListener('click', () => {
-      flipCard.classList.toggle('flipped')
-    })
-  })
-})
